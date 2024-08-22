f6530baab7a4520f6cd25adeb1ce5d481f2dd59a
Johnatan Freire <johnatanfreire09@gmail.com>
2024-08-21 22:24:12 -0300

    Validação do formulário

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -12,6 +12,26 @@ class ContactController extends Controller
     }
 
     public function store(Request $request){
+        $request->validate(
+            [
+                'name' => ['required', 'min:3'],
+                'email' => ['required', 'email'],
+                'phone'=> ['required', 'min:11', 'regex:/^\(\d{2}\) \d \d{4}-\d{4}$/'],
+                'subject' => ['required'],
+                'message' => ['required'],
+            ],
+            [
+                'name.required' => 'Preencha este campo.',
+                'name.min' => 'Isira um nome válido.',
+                'email.required' => 'Preencha este campo.',
+                'email.email' => 'Insira um e-mail válido.',
+                'phone.required' => 'Preencha este campo.',
+                'phone.min' => 'Insira um número para contato válido.',
+                'phone.regex' => 'Insira um número para contato válido.',
+                'subject.required' => 'Preencha este campo.',
+                'message.required' => 'Preenche este campo.',
+            ],
+        );
         Contact::create($request->all());
 
         return redirect()->route('site.index');
--- resources/views/site/layout/components/contact_form.blade.php
+++ resources/views/site/layout/components/contact_form.blade.php
@@ -4,27 +4,43 @@
     <div class="mx-4 my-3">
         <div class="col-12 mb-3">
             <b><label for="name"class="form-label">Nome</label></b>
-            <input type="text" class="form-control" id="name" name="name" placeholder="Julia Helen Martins"
-                required>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}"
+                placeholder="Julia Helen Martins">
+            @error('name')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="col-12 mb-3">
             <b><label for="email"class="form-label">E-mail</label></b>
-            <input type="email" class="form-control" id="email" name="email" placeholder="juliahelen@gmail.com" required>
+            <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}"
+                placeholder="juliahelen@gmail.com">
+            @error('email')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="col-12 mb-3">
             <b><label for="phone"class="form-label">Telefone</label></b>
-            <input type="tel" class="form-control telMask" id="phone" name="phone" placeholder="(61) 9 8653-1450" required>
+            <input type="tel" class="form-control telMask" id="phone" name="phone" value="{{ old('phone') }}"
+                placeholder="(61) 9 8653-1450">
             @error('phone')
-                <div class="text-danger">{{ $message }}</div>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="col-12 mb-3">
             <b><label for="subject"class="form-label">Assunto</label></b>
-            <input type="text" class="form-control" id="subject" name="subject" placeholder="Quero adquirir o sistema" required>
+            <input type="text" class="form-control" id="subject" name="subject" value="{{ old('subject') }}"
+                placeholder="Quero adquirir o sistema">
+            @error('subject')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="col-12 mb-3">
             <b><label for="message"class="form-label">Mensagem</label></b>
-            <textarea class="form-control messageMain" id="message" name="message" placeholder="Digite aqui sua mensagem..." required></textarea>
+            <textarea class="form-control messageMain" id="message" name="message" value="{{ old('message') }}"
+                placeholder="Digite aqui sua mensagem..."></textarea>
+            @error('message')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="col-12"><button type="submit" class="btn btn-primary">Enviar</button></div>
     </div>
