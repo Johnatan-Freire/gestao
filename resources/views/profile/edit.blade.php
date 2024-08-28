b4995976595252792063446e38f6907e91d139c8
Johnatan Freire <johnatanfreire09@gmail.com>
2024-08-28 10:40:40 -0300

    trocando tailwind da página editar perfil e do cabeçalho do Breeze por bootstrap

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,29 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10 col-lg-8 space-y-4">
+                <!-- Formulário de Atualização de Perfil -->
+                <div class="p-4 mb-4 bg-white shadow rounded">
+                    <div class="w-100">
+                        @include('profile.partials.update-profile-information-form')
+                    </div>
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+    
+                <!-- Formulário de Atualização de Senha -->
+                <div class="p-4 mb-4 bg-white shadow rounded">
+                    <div class="w-100">
+                        @include('profile.partials.update-password-form')
+                    </div>
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+    
+                <!-- Formulário de Exclusão de Conta -->
+                <div class="p-4 bg-white shadow rounded">
+                    <div class="w-100">
+                        @include('profile.partials.delete-user-form')
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    
 </x-app-layout>
--- resources/views/site/layout/partials/header.blade.php
+++ resources/views/site/layout/partials/header.blade.php
@@ -1,20 +1,34 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid"><a class="navbar-brand" href="{{ route('site.index') }}"><img
-                    src="{{ asset('images/logo.png') }}" alt="Logo da empresa" class="logo"></a><button
-                class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page"
-                            href="{{ route('site.index') }}">Início</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('site.about') }}">Sobre nós</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('site.contact') }}">Contato</a></li>
-                </ul>
-            </div>
+        <div class="container-fluid">
+            <!-- Logo -->
+            <a class="navbar-brand" href="{{ route('site.index') }}">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo da empresa" class="logo">
+            </a>
 
-            <a href="{{ route('login') }}" class="text-decoration-none text-black">Login</a>
+            <!-- Botão Toggle para dispositivos móveis -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Itens do Menu -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('site.index') }}">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('site.about') }}">Sobre nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('site.contact') }}">Contato</a>
+                    </li>
+                </ul>
+
+                <!-- Botão de Login -->
+                <a href="{{ route('login') }}" class="btn btn-primary ms-lg-3">Login</a>
+            </div>
         </div>
     </nav>
 </header>
