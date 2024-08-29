44eb0e099f4745332bce9e6db65a687a4a5f330e
Johnatan Freire <johnatanfreire09@gmail.com>
2024-08-29 16:29:07 -0300

    correção bug das mascaras cnpj e número de celular

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,17 @@ Alpine.start();
 
 import Inputmask from 'inputmask';
 
-document.addEventListener('DOMContentLoaded', function(){
-    var telMask = new Inputmask("(99) 9 9999-9999");
-    var cnpjMask = new Inputmask("99.999.999/9999-99");
-    
-    telMask.mask(document.querySelector('.telMask'));
-    cnpjMask.mask(document.querySelector('.cnpjMask'));
+document.addEventListener('DOMContentLoaded', function() {
+    var telElement = document.querySelector('.telMask');
+    var cnpjElement = document.querySelector('.cnpjMask');
+
+    if (telElement) {
+        var telMask = new Inputmask("(99) 9 9999-9999");
+        telMask.mask(telElement);
+    }
+
+    if (cnpjElement) {
+        var cnpjMask = new Inputmask("99.999.999/9999-99");
+        cnpjMask.mask(cnpjElement);
+    }
 });
